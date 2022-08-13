bcc0805747dbbeba2f4a60edce61c711758e10a3
daniszcode <daniele.marques@creditas.com>
2022-08-13 12:18:06 -0300

    💜 test: Adicionado teste unitário de valorHora💜

--- src/entregasDesafio/daniele-marques/valorHora.test.js
+++ src/entregasDesafio/daniele-marques/valorHora.test.js
@@ -1,10 +1,16 @@
-const { calcularValorPorHora } = require("./valorHora");
-
+const {
+  calcularValorPorHora,
+} = require("../../dominio/calculadora/Hora/valorHora");
 describe("Calculo valor hora", () => {
   test("O valor por hora de um salario de 1000 reais é 6r$/hora", () => {
     expect(calcularValorPorHora(1000)).toBe(6);
   });
+
+  test("Salário de 2000", () => {
+    expect(calcularValorPorHora(2000)).toBe(12);
+  });
 });
+
 describe("1 + 1", () => {
   test("adds 1 + 2 to equal 3", () => {
     expect(1 + 2).toBe(3);
