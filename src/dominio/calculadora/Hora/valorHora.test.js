e17f3ae2618618934671c5a9b5483285b9adb7d1
flareis <flavia.reis@dasa.com.br>
2022-08-13 11:23:14 -0300

    test: Hourly rate for a salary of 1000 reais

--- src/dominio/calculadora/Hora/valorHora.test.js
+++ src/dominio/calculadora/Hora/valorHora.test.js
@@ -7,3 +7,11 @@ test ('If 1 + 1 not equal 0', () => {
 test ('If 1 + 1 is equal 2', () => {
   expect(1+1).toBe(2)
 })
+
+test ('Valor/hora para um salário de 1000 reais', ()=>{
+  const salário = 1000
+  const valorPorHora = calcularValorPorHora(salário)
+  const resultadoEsperado = 6
+
+  expect(valorPorHora).toBe(resultadoEsperado)
+})
