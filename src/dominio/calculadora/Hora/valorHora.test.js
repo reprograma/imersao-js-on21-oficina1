1df30985977fbc833caf03612fa4efdc844d82cf
flareis <flavia.reis@dasa.com.br>
2022-08-13 11:29:24 -0300

    add describe for 1 + 1

--- src/dominio/calculadora/Hora/valorHora.test.js
+++ src/dominio/calculadora/Hora/valorHora.test.js
@@ -11,3 +11,13 @@ describe('Cálculo Valor Hora', () => {
     expect(calcularValorPorHora(1000)).toBe(6)
   })
 })
+
+describe ('1 + 1', () => {
+  test ('If 1 + 1 not equal 0', () => {
+    expect(1+1).not.toBe(0)
+  })
+  
+  test ('If 1 + 1 is equal 2', () => {
+    expect(1+1).toBe(2)
+  })
+})
