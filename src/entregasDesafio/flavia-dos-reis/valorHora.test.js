fdb645e41fcdbc7faec1fd18c4baa9b7e2a89c77
flareis <flavia.reis@dasa.com.br>
2022-08-13 12:01:23 -0300

    add folder for Flavia dos Reis

---
+++ src/entregasDesafio/flavia-dos-reis/valorHora.test.js
@@ -0,0 +1,23 @@
+const {calcularValorPorHora} = require("../../dominio/calculadora/Hora/valorHora")
+
+describe('Cálculo Valor Hora', () => {
+  
+  
+  test ('Valor/hora para um salário de R$1000 é R$ 6/hora', ()=>{
+    /* const salário = 1000
+    const valorPorHora = calcularValorPorHora(salário)
+    const resultadoEsperado = 6 */
+  
+    expect(calcularValorPorHora(1000)).toBe(6)
+  })
+})
+
+describe ('1 + 1', () => {
+  test ('If 1 + 1 not equal 0', () => {
+    expect(1+1).not.toBe(0)
+  })
+  
+  test ('If 1 + 1 is equal 2', () => {
+    expect(1+1).toBe(2)
+  })
+})
