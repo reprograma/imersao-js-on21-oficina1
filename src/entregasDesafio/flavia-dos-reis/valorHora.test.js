4b458787f4bb4daa4a0149e216b3e26e9e5cde6e
flareis <flavia.reis@dasa.com.br>
2022-08-13 19:38:32 -0300

    teste: cobertura de testes valor / hora - desafio

--- src/entregasDesafio/flavia-dos-reis/valorHora.test.js
+++ src/entregasDesafio/flavia-dos-reis/valorHora.test.js
@@ -1,37 +1,19 @@
-const {calcularValorPorHora} = require("./valorHora")
-const {horasTrabalhadasPorMes} = require("./valorHora")
-const {valorPorHora} = require("./valorHora")
+const {calcularValorPorHora, horasTrabalhadasPorMes, valorPorHora} = require("../../dominio/calculadora/Hora/valorHora")
 const {
   HORAS_POR_DIA,
   DIAS_UTEIS_NO_MES
-} = require('../constantes/constantes')
+} = require('../../dominio/calculadora/constantes/constantes')
 
 describe('Cálculo Valor Hora', () => {
   test ('Valor/hora para um salário de R$1000 é R$ 6/hora', () => {
     expect(calcularValorPorHora(1000)).toBe(6)
   })
 
-  test ('Deve retornar horas trabalhadas por mês', () => {
-    expect(HORAS_POR_DIA * DIAS_UTEIS_NO_MES).toBe(176)
+  test ('Qualquer valor por hora for negativo retornará renda mesnsal -0', () =>{
+    expect(calcularValorPorHora(-10)).toBe(-0)
   })
 
-  test('Deve retornar valor por hora', () => {
-    const rendaMensal = 4500;
-    const horasTrabalhadasPorMes = 40
-    const valorPorHora = rendaMensal / horasTrabalhadasPorMes
-    expect(valorPorHora).toBe(112.5)
-  })
-
-
-
-})
-
-describe ('1 + 1', () => {
-  test ('If 1 + 1 not equal 0', () => {
-    expect(1+1).not.toBe(0)
-  })
-  
-  test ('If 1 + 1 is equal 2', () => {
-    expect(1+1).toBe(2)
+  test ('Se valor por hora for zero retornará renda mesnsal 0', () =>{
+    expect(calcularValorPorHora(0)).toBe(0)
   })
 })
