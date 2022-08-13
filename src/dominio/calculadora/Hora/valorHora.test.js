2f20c1cdc5cccc037ec4738c51d18439718eed3c
flareis <flavia.reis@dasa.com.br>
2022-08-13 11:28:55 -0300

    refactor test: Hourly rate for a salary of 1000 reais

--- src/dominio/calculadora/Hora/valorHora.test.js
+++ src/dominio/calculadora/Hora/valorHora.test.js
@@ -1,17 +1,13 @@
 const {calcularValorPorHora} = require("./valorHora")
 
-test ('If 1 + 1 not equal 0', () => {
-  expect(1+1).not.toBe(0)
-})
-
-test ('If 1 + 1 is equal 2', () => {
-  expect(1+1).toBe(2)
-})
-
-test ('Valor/hora para um salário de 1000 reais', ()=>{
-  const salário = 1000
-  const valorPorHora = calcularValorPorHora(salário)
-  const resultadoEsperado = 6
-
-  expect(valorPorHora).toBe(resultadoEsperado)
+describe('Cálculo Valor Hora', () => {
+  
+  
+  test ('Valor/hora para um salário de R$1000 é R$ 6/hora', ()=>{
+    /* const salário = 1000
+    const valorPorHora = calcularValorPorHora(salário)
+    const resultadoEsperado = 6 */
+  
+    expect(calcularValorPorHora(1000)).toBe(6)
+  })
 })
