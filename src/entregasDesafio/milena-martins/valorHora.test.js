eec4f45acf141a420d6a22c66755778935234d78
flareis <flavia.reis@dasa.com.br>
2022-08-14 14:14:19 -0300

    teste em  Milena-Martins

--- src/entregasDesafio/milena-martins/valorHora.test.js
+++ src/entregasDesafio/milena-martins/valorHora.test.js
@@ -1,1 +1,7 @@
-// Testes aqui
+const {calcularValorPorHora} = require ("../../dominio/calculadora/Hora/valorHora")
+
+describe('Cálculo Valor Hora', () => {
+  test ('Valor por hora será de R$35 para um salário de R$3872 ', () => {
+    expect(calcularValorPorHora(6160)).toBe(35)
+  })
+})
