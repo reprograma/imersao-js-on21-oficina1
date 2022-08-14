45d0529178ed90286fd8de4c7ab2a914c7ae5fa9
Daviny Vidal <daviny@vidal.dev.br>
2022-08-14 17:02:55 -0300

    add test package test 'calcular Pacote'

--- src/entregasDesafio/daviny-vidal/valorProjeto.test.js
+++ src/entregasDesafio/daviny-vidal/valorProjeto.test.js
@@ -1,6 +1,20 @@
 const {calcularValorTotalProjeto, calcularValorBaseProjeto} = require('./valorProjeto')
+const { calcularHorasDeProjeto } = require('./horasPorProjeto');
+const { calcularPacote } = require('./pacote');
 
 describe("Valor total do Projeto", () => {
+
+    test("Calcular Hora de Projeto", ()=>{
+        const listaDeFuncionalidades = ["integracao_api_propria"];
+        const horasDeProjeto = calcularHorasDeProjeto(listaDeFuncionalidades)
+        expect(horasDeProjeto).toBe(16);
+    });
+
+    test("calcular Pacote", ()=>{
+        const horas = 104;
+        const pacote = calcularPacote(horas)
+        expect(pacote).toBe("pacote_premium")
+    });
 
     test("Calcular valor base => 6 * 211 ", ()=>{
         const valorHora = 6;
