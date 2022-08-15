eb3be810708c9009018e0715234c071ff418b5e2
sabrinaberno <sabrinaberno@hotmail.com>
2022-08-15 10:50:14 -0300

    Realizando os testes do desafio

--- src/entregasDesafio/sabrina-caldas-berno/pacote.test.js
+++ src/entregasDesafio/sabrina-caldas-berno/pacote.test.js
@@ -2,11 +2,25 @@ const { calcularPacote } = require('../../dominio/calculadora/Projeto/pacote')
 
 describe('Testar pacotes de acordo com as horas totais do projeto', () => {
 
-    test('testar o pacote básico', () => {
+    test('Testar o máximo de horas do pacote básico', () => {
         const horasProjeto = 50;
 
         const result = calcularPacote(horasProjeto)
         expect(result).toEqual('pacote_basico')
-    })
-   
+    });
+
+    test('Testar o máximo de horas do pacote intermediário', () => {
+        const horasProjeto = 100;
+
+        const result = calcularPacote(horasProjeto)
+        expect(result).toEqual('pacote_intermediario')
+    });
+
+    test('Testar o máximo de horas do pacote premium', () => {
+        const horasProjeto = 200;
+
+        const result = calcularPacote(horasProjeto)
+        expect(result).toEqual('pacote_premium')
+    });
+    
 })
--- src/entregasDesafio/sabrina-caldas-berno/pacotetest.test.js
+++ src/entregasDesafio/sabrina-caldas-berno/pacotetest.test.js
@@ -3,16 +3,11 @@ const pacote = require('../../dominio/calculadora/Projeto/pacote')
 
 jest.mock('../../dominio/calculadora/Projeto/pacote.js')
 
-//fazer para todos os pacotes -  extra
-//qual mock vc vai usar 
 
-describe(' Valor do Projeto ', () => {
-    beforeEach( () => {
-        pacote.calcularPacote.mockReturnValue('pacote_basico');
-       // pacote.calcularPacote.mockReturnValue('pacote_intermediario');
-    })
-    
+describe(' Valor do Projeto de acordo com o tipo de pacote ', () => {
+        
     test('Retorna o valor total para um projeto básico dada as funcionalidades pedidas e o valor da hora da desenvolvedora', () => {
+        pacote.calcularPacote.mockReturnValue('pacote_basico');
         const funcionalidades = [
             'setup',
             'responsividade',
@@ -22,6 +17,40 @@ describe(' Valor do Projeto ', () => {
         const valorHora = 70;
         const result = calcularValorTotalProjeto(funcionalidades, valorHora)
         expect(result).toEqual(2464)
+
+    });
+
+    test('Retorna o valor total para um projeto intermediário dada as funcionalidades pedidas e o valor da hora da desenvolvedora', () => {
+        pacote.calcularPacote.mockReturnValue('pacote_intermediario');
+        const funcionalidades = [
+            'responsividade',
+            'otimizacao_seo',
+            'construcao_1_pagina',
+            'integracao_mailchimp'
+        ];
+
+        const valorHora = 85;
+        const result = calcularValorTotalProjeto(funcionalidades, valorHora)
+        expect(result).toEqual(5331)
+
+    });
+
+    test('Retorna o valor total para um projeto premium dada as funcionalidades pedidas e o valor da hora da desenvolvedora', () => {
+        pacote.calcularPacote.mockReturnValue('pacote_premium');
+        const funcionalidades = [
+            'setup',
+            'formulario',
+            'responsividade',
+            'otimizacao_seo',
+            'construcao_1_pagina',
+            'integracao_mailchimp',
+            'ssr',
+            'integracao_api_propria'
+        ];
+
+        const valorHora = 50;
+        const result = calcularValorTotalProjeto(funcionalidades, valorHora)
+        expect(result).toEqual(5980)
 
     })
 })
